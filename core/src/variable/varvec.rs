afd612a862216c18baca2aef64ffaf528ac55ae0
Thomas Firmin <thomas.firmin@univ-lille.fr>
2025-05-08 21:07:52 +0200

    add varvec

--- core/src/variable/varvec.rs
+++ core/src/variable/varvec.rs
@@ -1,23 +1,21 @@
-pub struct VarVec;
+use crate::var::Var;
 
-// use crate::var::Var;
+pub struct VarVec
+{
+    variables : Vec<V>,
+    vlen:usize, // True length
+    tlen:usize, // Virtual length
+}
 
-// pub struct VarVec
-// {
-//     variables : Vec<>,
-//     vlen:usize, // True length
-//     tlen:usize, // Virtual length
-// }
+struct VarIter{
+    var : &Var<'a, Obj, Opt>,
+    varvec : &VarVec,
+    pos:(usize,usize),
+}
 
-// struct VarIter{
-//     var : &Var<'a, Obj, Opt>,
-//     varvec : &VarVec,
-//     pos:(usize,usize),
-// }
-
-// impl VarVec{
-//     fn iter(&self) -> VarIter{
-//         let first = self.variables.first().unwrap();
-//         VarIter { var: first, varvec:&self , pos: first }
-//     }
-// }
+impl VarVec{
+    fn iter(&self) -> VarIter{
+        let first = self.variables.first().unwrap();
+        VarIter { var: first, varvec:&self , pos: first }
+    }
+}
