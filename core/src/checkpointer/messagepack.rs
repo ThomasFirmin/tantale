1e0b13234ab4856d1fb77fddff92b41f37099e2b
Thomas Firmin <thomas.firmin@univ-lille.fr>
2026-04-08 14:31:53 +0200

    remove print

--- core/src/checkpointer/messagepack.rs
+++ core/src/checkpointer/messagepack.rs
@@ -34,7 +34,6 @@ fn load_from_path<SolId: Id, FnState: FuncState>(
 ) -> Result<(SolId, FnState), CheckpointError> {
     let path_id = &path.join(Path::new(&"id.mp"));
     if path_id.exists() {
-        println!("Loading FuncState from path: {}", path.display());
         let rdr = File::open(path_id).map_err(|e| CheckpointError(e.to_string()))?;
         let id_loaded: SolId = rmp_serde::decode::from_read(rdr).map_err(|e| CheckpointError(e.to_string()))?;
         let fn_state = FnState::load(path).map_err(|e| CheckpointError(e.to_string()))?;
