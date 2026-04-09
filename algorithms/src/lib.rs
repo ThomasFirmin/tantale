f638bfae9098083e15792a139b2d5fdf63d6128c
Thomas Firmin <thomas.firmin@univ-lille.fr>
2026-04-09 13:48:37 +0200

    doc(lib): Solved list issue

--- algorithms/src/lib.rs
+++ algorithms/src/lib.rs
@@ -12,10 +12,10 @@
 //! - [Asynchronous Successive Halving](asha) :
 //!   * [`Asha`] : the asynchronous version of [Successive Halving](https://arxiv.org/pdf/1810.05934) algorithm for multi-fidelity hyperparameter optimization.
 //! - [Hyperband](hyperband) :
-//!  * [`Hyperband`] : the original [Hyperband](https://arxiv.org/pdf/1603.06560) algorithm for multi-fidelity hyperparameter optimization.
+//!   * [`Hyperband`] : the original [Hyperband](https://arxiv.org/pdf/1603.06560) algorithm for multi-fidelity hyperparameter optimization.
 //! - [MO-ASHA](moasha) :
-//!  * [`MoAsha`] : the multi-objective version of ASHA, based on the [MO-ASHA](https://arxiv.org/pdf/2106.12639) algorithm for multi-fidelity
-//!    and multi-objective hyperparameter optimization.
+//!   * [`MoAsha`] : the multi-objective version of ASHA, based on the [MO-ASHA](https://arxiv.org/pdf/2106.12639) algorithm for multi-fidelity
+//!     and multi-objective hyperparameter optimization.
 //!
 
 pub mod random_search;
