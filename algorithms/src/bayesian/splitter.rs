f6f7c0fae87156a03c1d571703a43467b65c8a84
Thomas Firmin <thomas.firmin@univ-lille.fr>
2026-06-11 18:28:26 +0200

    feat(MOSplite): Added multi-objective splitter for MO splitter based on the WFG algorithm and Greedy Hypervolume subset selection.

--- algorithms/src/bayesian/splitter.rs
+++ algorithms/src/bayesian/splitter.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
+use tantale_core::Dominate;
 
-use crate::{bayesian::error::SplitError, utils::OrdArchive};
+use crate::{bayesian::error::SplitError, mo::IntoNonDominatedSorting, utils::{OrdArchive, hypervolume::{wfg_hv, wfg_hv_extra, wfg_inclhv, worse_from_two}}};
 
 /// The [`Splitter`] trait defines a method for splitting a vector into two parts within the TPE algorithm.
 pub trait Splitter
@@ -89,5 +90,111 @@ impl Splitter for SqrtSplit {
         }
         let quantile = quantile as usize;
         (&archive.points[quantile..], &archive.points[..quantile])
+    }
+}
+
+/// The [`Splitter`] trait defines a method for splitting a vector into two parts within the TPE algorithm.
+pub trait MOSplitter
+where
+    Self: Sized + Serialize + for<'a> Deserialize<'a>,
+{
+    /// Splits the given archive of points into two parts: the "good" set and the "bad" set.
+    /// The "good" set contains the best points, while the "bad" set contains the worst points.
+    /// The split is typically based on a quantile of the archive, which determines the fraction of points that belong to the "good" set.
+    ///
+    /// # Parameters
+    /// - `archive`: A reference to a [`OrdArchive`] containing the observed points, sorted in ascending order by their objective values.
+    ///
+    /// # Returns
+    /// A tuple containing two slices: the first slice corresponds to the "good" set (the best points), and the second slice corresponds to the "bad" set (the worst points).
+    fn split<'a, T: Dominate + Serialize + for<'de> Deserialize<'de>>(
+        &self,
+        archive: &'a [T],
+    ) -> (Vec<&'a T>, Vec<&'a T>);
+}
+
+
+pub fn reference_point<T:Dominate>(archive: &[T]) -> Vec<f64> {
+    let num_objectives = archive[0].get_max_objectives();
+    (0..num_objectives).map(
+        |idx |
+        {
+            archive.iter().map(|point| point.get_objective_by_index(idx)).fold(f64::NEG_INFINITY, f64::max)
+        }
+    ).collect()
+}
+
+pub fn greedy_hss<'a, T, U>(front: &mut Vec<&'a T>, n:usize, ref_point: &U) -> Vec<&'a T>
+where
+    T: Dominate,
+    U: Dominate,
+{
+    let mut extracted = Vec::new();
+
+    let mut best_contrib = front
+        .iter()
+        .map(|point| wfg_inclhv(*point, ref_point))
+        .enumerate()
+        .max_by(|(_, a), (_, b)| a.total_cmp(b));
+    
+    while let Some((idx, _)) = best_contrib && extracted.len() < n {
+        let selected = front.remove(idx);
+        let hv_indic_extracted = wfg_hv(&extracted, ref_point);
+        
+        best_contrib = front
+            .iter()
+            .map(
+                |point|
+                {
+                    let worse = worse_from_two(selected, *point);
+                    wfg_hv_extra(&extracted, &worse, ref_point) - hv_indic_extracted
+                }
+            )
+            .enumerate()
+            .max_by(|(_, a), (_, b)| a.total_cmp(b));
+        extracted.push(selected);
+    }
+    extracted
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MOSplit(pub f64);
+
+impl MOSplitter for MOSplit {
+    fn split<'a, T: Dominate + Serialize + for<'de> Deserialize<'de>>(
+        &self,
+        archive: &'a [T],
+    ) -> (Vec<&'a T>, Vec<&'a T>) {
+        let quantile = (archive.len() as f64 * self.0).ceil() as usize;
+
+        let ref_archive = archive.iter().collect::<Vec<&T>>();
+        let mut fronts = ref_archive.into_non_dominated_sort();
+
+        let worst_point = reference_point(archive);
+
+        let mut good = Vec::new();
+        let mut bad = Vec::new();
+
+        let mut i = 0;
+        while i < fronts.len() && good.len() + fronts[i].len() < quantile {
+            good.extend_from_slice(&fronts[i]);
+            i += 1;
+        }
+
+        if i < fronts.len() && good.len() < quantile {
+            let remaining = quantile - good.len();
+
+            let extracted = greedy_hss(&mut fronts[i], remaining, &worst_point);
+
+            good.extend(extracted);
+            bad.extend_from_slice(&fronts[i]);
+            i += 1;
+        }
+
+        for f in &fronts[i..] {
+            bad.extend_from_slice(f);
+        }
+
+        (good, bad)
     }
 }
--- algorithms/src/utils/hypervolume.rs
+++ algorithms/src/utils/hypervolume.rs
@@ -1,46 +1,89 @@
-use tantale_core::{Dominate, HasY, Multi, Outcome, TypeCodom};
+use tantale_core::Dominate;
 
-pub fn wfg_inclhv<T, Out>(point: &T, ref_point: &TypeCodom<Out>, n_obj: usize) -> f64
-where 
-    T : Dominate + HasY<Out>,
-    Out : Outcome,
-    Out::Cod: Multi<Out>,
-    TypeCodom<Out>: Dominate,
-{
-    (0..n_obj).map(
-        |idx| 
-        {   
-            (point.get_objective_by_index(idx) - ref_point.get_objective_by_index(idx)).abs()
-        }
-    ).product()
+use crate::mo::ParetoFront;
+
+/// Represents the errors that can occur when computing the WFG hypervolume indicator.
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
+#[non_exhaustive]
+pub enum WFGError {
+    ObjectivesInvalid,
 }
 
-pub fn wfg_limits<T, Out>(front: &[T], k:usize, n_obj: usize) -> Vec<Vec<f64>>
-where
-    T: Dominate + HasY<Out>,
-    Out: Outcome,
-    TypeCodom<Out>: Dominate,
-{
-    let lenmk = front.len() - k;
-    let pivot = &front[lenmk];
-    front[..lenmk].iter().map(
-        |sol|{
-            (0..n_obj).map(
-                |j|
-                pivot.get_objective_by_index(j).min(sol.get_objective_by_index(j))
-            ).collect()
+impl std::fmt::Display for WFGError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            WFGError::ObjectivesInvalid => write!(f, "Inputs have different number of objectives"),
         }
+    }
+}
+
+pub fn worse_from_two<T: Dominate, U: Dominate>(a: &T, b: &U) -> Vec<f64> {
+    a.iter_obj()
+        .zip(b.iter_obj())
+        .map(|(x, y)| x.min(y))
+        .collect()
+}
+
+pub fn wfg_limits<T: Dominate>(front: &[&T], k:usize) -> Vec<Vec<f64>>
+{
+    let pivot = front[k];
+    front[k+1..].iter().map(
+        |sol| worse_from_two(pivot, *sol)
     ).collect()
 }
 
-pub fn wfg_exclhv<T, Out>(front: &[T], ref_point: &TypeCodom<Out>, k:usize, n_obj: usize) -> Vec<Vec<f64>>
-where
-    T: Dominate + HasY<Out>,
-    Out: Outcome,
-    TypeCodom<Out>: Dominate,
+pub fn wfg_inclhv<T: Dominate, U: Dominate>(point: &T, ref_point: &U) -> f64
 {
-    let point = &front[k];
-    // let inclusive = wfg_inclhv(point, ref_point, n_obj);
-    let limits = wfg_limits(front, k, n_obj);
-    todo!()
+    point.iter_obj().zip(ref_point.iter_obj()).map(
+        |(y1, y2)| (y1 - y2).abs()
+    ).product()
+}
+
+
+pub fn wfg_exclhv<T: Dominate, U: Dominate>(front: &[&T], ref_point: &U, k:usize) ->f64
+{
+    let limits = wfg_limits(front, k);
+    let pareto = limits.pareto_front();
+    let inclusive = wfg_inclhv(front[k], ref_point);
+    let wfg =  wfg_hv(&pareto, ref_point);
+    inclusive - wfg
+}
+
+pub fn wfg_hv<T: Dominate, U: Dominate>(front: &[&T], ref_point: &U) -> f64
+{
+    (0..front.len()).map(|k| wfg_exclhv(front, ref_point, k)).sum()
+}
+
+
+
+pub fn wfg_limits_extra<T: Dominate, E: Dominate>(front: &[&T], extra: &E, k:usize) -> Vec<Vec<f64>>
+{
+    if k == front.len(){
+        Vec::new()
+    } else {
+        let pivot = front[k];
+        let mut res: Vec<Vec<f64>> = front[k+1..].iter().map(
+            |sol| worse_from_two(pivot, *sol)
+        ).collect();
+        res.push(worse_from_two(pivot, extra));
+        res
+    }
+}
+
+pub fn wfg_exclhv_extra<T: Dominate, E:Dominate, U: Dominate>(front: &[&T], extra: &E, ref_point: &U, k:usize) ->f64
+{
+    let limits = wfg_limits_extra(front, extra, k);
+    let pareto = limits.pareto_front();
+    let inclusive = if k == front.len() {
+         wfg_inclhv(extra, ref_point)
+    } else {
+         wfg_inclhv(front[k], ref_point)
+    };
+    let wfg =  wfg_hv(&pareto, ref_point);
+    inclusive - wfg
+}
+
+pub fn wfg_hv_extra<T: Dominate, E:Dominate, U: Dominate>(front: &[&T], extra: &E, ref_point: &U) -> f64
+{
+    (0..front.len()+1).map(|k| wfg_exclhv_extra(front, extra, ref_point, k)).sum()
 }
