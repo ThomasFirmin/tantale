06ccbfe54c1d2e7a2ed4f229867248a714b8c2d6
Thomas Firmin <thomas.firmin@univ-lille.fr>
2026-07-07 10:50:32 +0200

    feat(XToNdArray): Added `x_array_type` to XToNdArray, allowing to to convert the object into an array of the desired type, if the TypeDom is AsPrimitive<T>.

--- core/src/utils/xy.rs
+++ core/src/utils/xy.rs
@@ -13,6 +13,7 @@ use crate::{
 };
 
 use ndarray::{Array1, Array2, Axis, Zip};
+use num::cast::AsPrimitive;
 use serde::{Deserialize, Serialize};
 use std::{cmp::Ordering, sync::Arc};
 
@@ -21,6 +22,13 @@ use std::{cmp::Ordering, sync::Arc};
 pub struct Xy<Raw, Y> {
     pub x: Raw,
     pub y: Arc<Y>,
+}
+
+impl <Raw, Y> Xy<Raw, Y> {
+    /// Creates a new `Xy` instance with the given raw solution and computed codomain value.
+    pub fn new(x: Raw, y: Arc<Y>) -> Self {
+        Self { x, y }
+    }
 }
 
 impl<Raw: Clone, Y> HasX<Raw> for Xy<Raw, Y> {
@@ -91,6 +99,17 @@ impl<Raw, Y: Dominate> Dominate for Xy<Raw, Y> {
 pub trait XToNdArray<Dom: Domain> {
     /// Returns the raw solution data as a 2D array, where each row corresponds to a solution and each column corresponds to a dimension of the solution space.
     fn x_array(&self) -> Array2<Dom::TypeDom>;
+
+    /// Returns the raw solution data as a 2D array of a specified type `T`, 
+    /// where each row corresponds to a solution and each column corresponds to a dimension of the solution space.
+    ///
+    /// # Type Parameters
+    /// * `T`: The desired type for the elements of the resulting array. It must implement `Copy` and be convertible from `Dom::TypeDom` using the `AsPrimitive` trait.
+    fn x_array_type<T>(&self) -> Array2<T> 
+    where
+        T: Copy + 'static,
+        Dom::TypeDom: AsPrimitive<T>;
+
     /// Returns the number of columns in the resulting `Array2`, which corresponds to the dimensionality of the raw solution data.
     fn x_cols(&self) -> usize;
 }
@@ -107,29 +126,12 @@ where
     fn x_cols(&self) -> usize {
         self.x.len()
     }
-}
-
-impl<SolId, Dom, SInfo> XToNdArray<Dom> for [BaseSol<SolId, Dom, SInfo>]
-where
-    Dom: Domain,
-    SInfo: SolInfo,
-    SolId: Id,
-{
-    fn x_array(&self) -> Array2<Dom::TypeDom> {
-        let n_rows = self.len();
-        let n_cols = self[0].ref_x().len();
-        let mut array = Array2::<Dom::TypeDom>::uninit((n_rows, n_cols));
-        Zip::from(array.rows_mut())
-            .and(self)
-            .for_each(|mut row, sol| {
-                for (idx, x) in sol.ref_x().iter().enumerate() {
-                    row[idx].write(x.clone());
-                }
-            });
-        unsafe { array.assume_init() }
-    }
-    fn x_cols(&self) -> usize {
-        self[0].ref_x().len()
+    
+    fn x_array_type<T>(&self) -> Array2<T> 
+    where
+        T: Copy + 'static,
+        Dom::TypeDom: AsPrimitive<T> {
+        Array1::from_iter(self.x.iter().map(|x| x.as_())).insert_axis(Axis(0))
     }
 }
 
@@ -145,29 +147,12 @@ where
     fn x_cols(&self) -> usize {
         self.x.len()
     }
-}
 
-impl<SolId, Dom, SInfo> XToNdArray<Dom> for [FidelitySol<SolId, Dom, SInfo>]
-where
-    Dom: Domain,
-    SInfo: SolInfo,
-    SolId: StepId,
-{
-    fn x_array(&self) -> Array2<Dom::TypeDom> {
-        let n_rows = self.len();
-        let n_cols = self[0].ref_x().len();
-        let mut array = Array2::<Dom::TypeDom>::uninit((n_rows, n_cols));
-        Zip::from(array.rows_mut())
-            .and(self)
-            .for_each(|mut row, sol| {
-                for (idx, x) in sol.ref_x().iter().enumerate() {
-                    row[idx].write(x.clone());
-                }
-            });
-        unsafe { array.assume_init() }
-    }
-    fn x_cols(&self) -> usize {
-        self[0].ref_x().len()
+    fn x_array_type<T>(&self) -> Array2<T> 
+    where
+        T: Copy + 'static,
+        Dom::TypeDom: AsPrimitive<T> {
+        Array1::from_iter(self.x.iter().map(|x| x.as_())).insert_axis(Axis(0))
     }
 }
 
@@ -186,24 +171,49 @@ where
     fn x_cols(&self) -> usize {
         self.sol.x_cols()
     }
+
+    fn x_array_type<T>(&self) -> Array2<T> 
+    where
+        T: Copy + 'static,
+        Dom::TypeDom: AsPrimitive<T> {
+        self.sol.x_array_type::<T>()
+    }
 }
 
-impl<PSol, SolId, Dom, Info, Out> XToNdArray<Dom> for [Computed<PSol, SolId, Dom, Out, Info>]
+impl<Dom, X> XToNdArray<Dom> for &X
 where
-    PSol: Uncomputed<SolId, Dom, Info> + XToNdArray<Dom>,
     Dom: Domain,
-    Info: SolInfo,
-    Out: Outcome,
-    SolId: Id,
+    X: XToNdArray<Dom> + ?Sized,
+{
+    fn x_array(&self) -> Array2<Dom::TypeDom> {
+        (*self).x_array()
+    }
+
+    fn x_cols(&self) -> usize {
+        (*self).x_cols()
+    }
+
+    fn x_array_type<T>(&self) -> Array2<T> 
+    where
+        T: Copy + 'static,
+        Dom::TypeDom: AsPrimitive<T> {
+        (*self).x_array_type::<T>()
+    }
+}
+
+impl<Dom, X> XToNdArray<Dom> for [X]
+where
+    X: XToNdArray<Dom>,
+    Dom: Domain,
 {
     fn x_array(&self) -> Array2<Dom::TypeDom> {
         let n_rows = self.len();
-        let n_col = self[0].sol.x_cols();
+        let n_col = self[0].x_cols();
         let mut array = Array2::<Dom::TypeDom>::uninit((n_rows, n_col));
         Zip::from(array.rows_mut())
             .and(self)
             .for_each(|mut row, sol| {
-                let x_array = sol.sol.x_array();
+                let x_array = sol.x_array();
                 for (i, val) in x_array.iter().enumerate() {
                     row[i].write(val.clone());
                 }
@@ -212,7 +222,69 @@ where
     }
 
     fn x_cols(&self) -> usize {
-        self[0].sol.x_cols()
+        self[0].x_cols()
+    }
+
+    fn x_array_type<T>(&self) -> Array2<T> 
+    where
+        T: Copy + 'static,
+        Dom::TypeDom: AsPrimitive<T>
+    {
+        let n_rows = self.len();
+        let n_col = self[0].x_cols();
+        let mut array = Array2::<T>::uninit((n_rows, n_col));
+        Zip::from(array.rows_mut())
+            .and(self)
+            .for_each(|mut row, sol| {
+                let x_array = sol.x_array_type::<T>();
+                for (i, val) in x_array.iter().enumerate() {
+                    row[i].write(*val);
+                }
+            });
+        unsafe { array.assume_init() }
+    }
+}
+
+impl <Dom> XToNdArray<Dom> for Arc<[Dom::TypeDom]>
+where
+    Dom: Domain,
+{
+    fn x_array(&self) -> Array2<Dom::TypeDom> {
+        Array1::from_iter(self.iter().cloned()).insert_axis(Axis(0))
+    }
+
+    fn x_cols(&self) -> usize {
+        self.len()
+    }
+
+    fn x_array_type<T>(&self) -> Array2<T> 
+    where
+        T: Copy + 'static,
+        Dom::TypeDom: AsPrimitive<T>
+    {
+        Array1::from_iter(self.iter().map(|x| x.as_())).insert_axis(Axis(0))
+    }
+}
+
+impl<Dom, X, Y> XToNdArray<Dom> for Xy<X, Y>
+where
+    Dom: Domain,
+    X: XToNdArray<Dom>,
+{
+    fn x_array(&self) -> Array2<Dom::TypeDom> {
+        self.x.x_array()
+    }
+
+    fn x_cols(&self) -> usize {
+        self.x.x_cols()
+    }
+
+    fn x_array_type<T>(&self) -> Array2<T> 
+    where
+        T: Copy + 'static,
+        Dom::TypeDom: AsPrimitive<T>
+    {
+        self.x.x_array_type::<T>()
     }
 }
 
